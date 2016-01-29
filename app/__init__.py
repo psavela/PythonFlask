852a997b415caee3596c0fee32da07452b5d2038
psavela <petri.m.savela@gmail.com>
2016-01-29 14:53:17 +0200

    29.01.2016

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 #Näin pystyy conffaan suoraan ilman erillistä config fileä
@@ -7,4 +8,5 @@ app = Flask(__name__)
 #This lines configures our app using the config.py file
 app.config.from_object('config')
 bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
 from app import routers
