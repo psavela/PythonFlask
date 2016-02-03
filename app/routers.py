0fcea1aec8683ac54bb06e0ee018bb21ad3af383
psavela <petri.m.savela@gmail.com>
2016-02-03 20:57:10 +0200

    03022016_2

--- app/routers.py
+++ app/routers.py
@@ -1,10 +1,11 @@
 from app import app
 
 #render_template gives you access to Jinj2 template
-from flask import render_template,request, make_response,flash,redirect,session
-from app.forms import LoginForm, RegisterForm, FriendForm
-from app import db
+from flask import render_template,request,make_response,flash,redirect,session
+from app.forms import LoginForm,RegisterForm,FriendForm
 from app.db_models import Users,Friends
+from app import db
+
 
 @app.route('/',methods=['GET','POST'])
 def index():
@@ -23,9 +24,9 @@ def index():
                 session['user_id'] = user[0].id
                 session['isLogged'] = True
                 #tapa1
-                friend = Friends.query.filter_by(user_id=user[0].id)
-                print(friend)
-                return render_template('template_user.html',isLogged=True,friends=friend)
+                friends = Friends.query.filter_by(user_id=user[0].id)
+                print(friends)
+                return render_template('template_user.html',isLogged=True,friends=friends)
             else:
                 flash('Wrong email or password')
                 return render_template('template_index.html',form=login,isLogged=False)
@@ -50,7 +51,7 @@ def registerUser():
                 db.session.rollback()
                 flash('Username allready in use')
                 return render_template('template_register.html',form=form,isLogged=False)
-            flash("Name {0} registered".format(form.email.data))
+            flash("Name {0} registered.".format(form.email.data))
             return redirect('/')
         else:
             flash('Invalid email address or no password given')
@@ -70,24 +71,26 @@ def userParams():
 
 
 @app.route('/friends',methods=['GET','POST'])
-def addFriend():
-    if not('isLogged' in session) or (session['isLogged'] == False):
-        return redirect('/')
-    form = FriendForm()
-    if request.method == 'GET':
-        return render_template('template_friends.html',form=form,isLogged=True)
-    else:
-        if form.validate_on_submit():
-            temp = Friends(form.name.data,form.address.data,form.age.data,session['user_id'])
-            db.session.add(temp)
-            db.session.commit()
-            #tapa2
-            user = Users.query.get(session['user_id'])
-            print(user.friends)
-            return render_template('template_user.html',isLogged=True,friends=user.friends)
-        else:
-            flash('Give proper values to all fields')
-            return render_template('template_friends.html',form=form,isLogged=True)
+def friends():
+	#Check that user has logged in before you let execute this route
+	if not('isLogged' in session) or (session['isLogged'] == False):
+		return redirect('/')
+	form = FriendForm()
+	if request.method == 'GET':
+		return render_template('template_friends.html',form=form,isLogged=True)
+	else:
+		if form.validate_on_submit():
+			temp = Friends(form.name.data,form.address.data,form.age.data,session['user_id'])
+			db.session.add(temp)
+			db.session.commit()
+			#tapa 2
+			user = Users.query.get(session['user_id'])
+			print(user.friends)
+			return render_template('template_user.html',isLogged=True,friends=user.friends)
+		else:
+			flash('Give proper values to all fields')
+			return render_template('template_friends.html',form=form,isLogged=True)
+	
 
 @app.route('/logout')
 def logout():
